f6636977b57bafb459c02f7b99dd1dac024c786c
hari-testpress <hari@testpress.in>
2023-10-16 20:13:29 +0530

    feat: Add SDK initializer class

--- lib/tpstreams_player_sdk.dart
+++ lib/tpstreams_player_sdk.dart
@@ -1,3 +1,33 @@
+library tpstreams_player_sdk;
 
+enum PROVIDER {
+  tpstreams,
+  testpress,
+}
 
-class TpstreamsPlayerSdk {}
+class TPStreamsSDK {
+  static String? _orgCode;
+  static PROVIDER? _provider;
+
+  static void initialize(
+      {PROVIDER provider = PROVIDER.tpstreams, required String orgCode}) {
+    if (orgCode.isEmpty) {
+      throw Exception("Given OrgCode is empty, please pass a valid orgCode");
+    }
+
+    _orgCode = orgCode;
+    _provider = provider;
+  }
+
+  static String get orgCode {
+    if (_orgCode == null) {
+      throw Exception("OrgCode has not been initialized");
+    }
+
+    return _orgCode!;
+  }
+
+  static bool get isInitialized => _orgCode != null;
+
+  static PROVIDER? get provider => _provider;
+}
