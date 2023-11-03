fa5936e9713490a26aa619f368aa7d3616df3648
hari-testpress <hari@testpress.in>
2023-11-03 17:31:13 +0530

    fix: Audio playing in background after exiting from player widget

--- ios/Classes/NativePlayerView.swift
+++ ios/Classes/NativePlayerView.swift
@@ -5,6 +5,7 @@ import TPStreamsSDK
 class NativePlayerView: NSObject, FlutterPlatformView {
     var player: TPAVPlayer!
     var playerViewController: TPStreamPlayerViewController?
+    var methodChannel: FlutterMethodChannel
 
     func view() -> UIView {
         return playerViewController?.view ?? UIView()
@@ -16,6 +17,23 @@ class NativePlayerView: NSObject, FlutterPlatformView {
             playerViewController = TPStreamPlayerViewController()
             playerViewController!.player = player
         }
+        methodChannel = FlutterMethodChannel(name: "tpstreams_player_sdk/player_view_\(viewId)", binaryMessenger: messenger)
         super.init()
+        methodChannel.setMethodCallHandler(onMethodCall)
+        
+    }
+    
+    func onMethodCall(call: FlutterMethodCall, result: FlutterResult) {
+        switch(call.method){
+        case "dispose":
+            self.dispose()
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    func dispose(){
+        self.player?.replaceCurrentItem(with: nil)
+        self.player = nil
     }
 }
