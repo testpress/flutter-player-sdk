9ed5a9c67c4b10e8bd49eaa40f73c78b8ad6fc93
hari-testpress <hari@testpress.in>
2023-12-29 09:05:28 +0530

    Refactor the code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      home: Scaffold(
       appBar: AppBar(
         title: const Text('Plugin example app'),
       ),
