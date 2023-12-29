37dfff8804d2ce066252310fb6a1e9a2706e1d28
hari-testpress <hari@testpress.in>
2023-12-29 18:21:36 +0530

    Refactor the code

--- lib/player_controller.dart
+++ lib/player_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:tpstreams_player_sdk/methods.dart';
 
@@ -35,6 +37,8 @@ class TPStreamsPlayerController {
   }
 
   Future<void> dispose() async {
-    await _channel!.invokeMethod(Methods.dispose);
+    if (Platform.isIOS) {
+      await _channel?.invokeMethod(Methods.dispose);
+    }
   }
 }
