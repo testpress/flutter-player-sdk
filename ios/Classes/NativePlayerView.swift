7ba5a82d610007488ba202ff814471796c9d79c2
hari-testpress <hari@testpress.in>
2025-01-06 16:02:00 +0530

    Refactor the code

--- ios/Classes/NativePlayerView.swift
+++ ios/Classes/NativePlayerView.swift
@@ -27,10 +27,9 @@ class NativePlayerView: NSObject, FlutterPlatformView, NativePlayerApi {
     }
 
     init(frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?, binaryMessenger messenger: FlutterBinaryMessenger) {
-        if let args = args as? [String: String], let assetId = args["assetId"] as? String, let accessToken = args["accessToken"] as? String {
+        if let args = args as? [String: Any], let assetId = args["assetId"] as? String, let accessToken = args["accessToken"] as? String {
             player = TPAVPlayer(assetID: assetId, accessToken: accessToken)
             playerViewController = TPStreamPlayerViewController()
-            configurePlayerViewController(args: args)
             playerViewController!.player = player
         }
         self.viewId = viewId
@@ -40,12 +39,15 @@ class NativePlayerView: NSObject, FlutterPlatformView, NativePlayerApi {
         super.init()
 
         NativePlayerApiSetup.setUp(binaryMessenger: messenger, api: self, messageChannelSuffix: "\(viewId)")
+        configurePlayerViewController(args: args)
         
         self.observePlayerStatusChange()
         self.observeCurrentItemChanges()
     }
     
-    private func configurePlayerViewController(args: [String: String]) {
+    private func configurePlayerViewController(args: Any?) {
+        guard let args = args as? [String: Any] else { return }
+        
         let showDownloadOption = (args["showDownloadOption"] as? Bool) ?? false
         if showDownloadOption {
             let config = TPStreamPlayerConfigurationBuilder()
