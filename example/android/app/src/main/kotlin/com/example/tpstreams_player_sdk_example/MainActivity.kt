2bb75c3e563f8b79f2e84b7edaeab959e5fb292f
Syed Ibrahim <syedibrahim@Syeds-Mac-mini.local>
2025-09-11 11:36:09 +0530

    fix example project

--- example/android/app/src/main/kotlin/com/example/tpstreams_player_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tpstreams_player_sdk_example
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -2,10 +2,11 @@ import Flutter
 import UIKit
 import XCTest
 
-@testable import tpstreams_player_sdk
+class RunnerTests: XCTestCase {
 
-// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
-//
-// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  func testExample() {
+    // If you add code to the Runner application, consider adding tests here.
+    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
 
-class RunnerTests: XCTestCase {}
+}
