24c9cc085a6f00e9622c8e001b9a47ba5ebbb812
Balamurugan-Testpress <balamurugan@testpress.in>
2026-07-28 17:54:36 +0530

    update the iOS version to 1.2.35

--- ios/tpstreams_player_sdk/Package.swift
+++ ios/tpstreams_player_sdk/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../FlutterFramework"),
-        .package(url: "https://github.com/testpress/iOSPlayerSDK.git", from: "1.2.29")
+        .package(url: "https://github.com/testpress/iOSPlayerSDK.git", from: "1.2.35")
     ],
     targets: [
         .target(
