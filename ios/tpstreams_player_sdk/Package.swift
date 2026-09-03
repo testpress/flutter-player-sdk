dcdaa6db205767fe7816c07667e76c1d64a9cdf0
syed-tp <syed@testpress.in>
2026-09-03 10:52:19 +0530

    chore: update native SDK versions (Android 1.2.10, iOS 1.2.42)

--- ios/tpstreams_player_sdk/Package.swift
+++ ios/tpstreams_player_sdk/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../FlutterFramework"),
-        .package(url: "https://github.com/testpress/iOSPlayerSDK.git", from: "1.2.41")
+        .package(url: "https://github.com/testpress/iOSPlayerSDK.git", from: "1.2.42")
     ],
     targets: [
         .target(
